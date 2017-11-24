929b261b3eb2d8bf11103f623b59586a0f894272
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-11-24 16:15:00 +0530

    Upload files to Google Cloud

--- Backend/api/RestControllers/imageController.js
+++ Backend/api/RestControllers/imageController.js
@@ -1,4 +1,8 @@
 const multer = require('multer');
+const Storage = require('@google-cloud/storage');
+const bucketName = 'medium-images';
+const cloudStorage = new Storage();
+
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './public')
@@ -10,8 +14,20 @@ var storage = multer.diskStorage({
 
 const upload = multer({ storage: storage })
 
+
 module.exports = function (app) {
   app.post('/upload', upload.single('avatar'), function (req, res) {
-    res.send(req.file)
+      cloudStorage
+          .bucket(bucketName)
+          .upload(req.file.path)
+          .then(() => {
+              console.log(`uploaded to ${bucketName}.`);
+              res.send(req.file);
+          })
+          .catch(err => {
+              console.error('ERROR:', err);
+              res.send(err);
+          });
+
   });
 };
--- Backend/constants.js
+++ Backend/constants.js
@@ -6,8 +6,8 @@ function getTimeToRead(content) {
 }
 
 module.exports = {
-    mongoURL: 'mongodb://localhost:27017',
-    PORT: 3000,
+    mongoURL: 'mongodb://admin:root@ds119736.mlab.com:19736/ng-medium',
+    PORT: process.env.PORT || 3000,
     INTERNAL_SERVER_ERROR: 500,
     STATUS_ERROR: 0,
     STATUS_SUCCESS: 1,
