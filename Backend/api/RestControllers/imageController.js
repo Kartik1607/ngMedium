84a989bb925723eeaab21a388fee93277574da6f
Kartik Sharma <kartik.sharma1607@outlook.com>
2017-11-24 21:45:31 +0530

    Migrated to Google Cloud Storage

--- Backend/api/RestControllers/imageController.js
+++ Backend/api/RestControllers/imageController.js
@@ -4,30 +4,36 @@ const bucketName = 'medium-images';
 const cloudStorage = new Storage();
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public')
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
+    destination: function (req, file, cb) {
+        cb(null, './public')
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + '-' + file.originalname)
+    }
 });
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 
 module.exports = function (app) {
-  app.post('/upload', upload.single('avatar'), function (req, res) {
-      cloudStorage
-          .bucket(bucketName)
-          .upload(req.file.path)
-          .then(() => {
-              console.log(`uploaded to ${bucketName}.`);
-              res.send(req.file);
-          })
-          .catch(err => {
-              console.error('ERROR:', err);
-              res.send(err);
-          });
+    app.post('/upload', upload.single('avatar'), function (req, res) {
+        cloudStorage
+            .bucket(bucketName)
+            .upload(req.file.path)
+            .then(()=>{
+                cloudStorage.bucket(bucketName)
+                    .file(req.file.filename)
+                    .makePublic()
+                    .then(() => {
+                        console.log(`uploaded to ${bucketName}.`);
+                        res.send(`https://storage.googleapis.com/${bucketName}/${req.file.filename}`);
+                    })
+                    .catch(err => {
+                        console.error('ERROR:', err);
+                        res.send(err);
+                    });
+            });
 
-  });
+
+    });
 };
